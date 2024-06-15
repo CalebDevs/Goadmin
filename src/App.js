db86ad826e7ffe83d4d4bd89747be32b64f7a17c
CalebDevs <cal.mithamo@gmail.com>
2024-06-15 03:16:29 +0000

    sidebar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { DefaultSidebar } from './components/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <DefaultSidebar/>
+      
+    </div>
+  );
+}
+
+export default App;
